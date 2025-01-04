a7b8bbf7c9f40ea45120f5262f68d14b55f7fec4
Chaudhari-Ankita <chaudhariankita1410@gmail.com>
2025-01-03 19:17:52 -0500

    web page changes and images folder added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Chaudhari-Ankita/AC-Projects/Resin-Art-Portofolio-using-Docker
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./Templates")) // created a variable 'fs' that points to the "./templates" directory.
+	http.Handle("/", fs)
+	log.Fatal(http.ListenAndServe(":80", nil))
+
+}
